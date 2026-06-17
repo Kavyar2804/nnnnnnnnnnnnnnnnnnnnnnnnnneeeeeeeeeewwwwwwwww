e0501dc9ef6706231677d1f4509bd783a05819fa
Kavyar2804 <kavyar2804@gmail.com>
2026-06-17 13:00:40 +0530

    First commit

---
+++ test/specs/sample.spec.js
@@ -0,0 +1,14 @@
+
+
+describe('Name of the suite', () => {
+
+    it('first', async() => {
+
+        await browser.url('https://demoapps.qspiders.com/ui?scenario=1')
+        await browser.$('#name').setValue('vhjhj')
+        await browser.
+
+
+    });
+    
+});
